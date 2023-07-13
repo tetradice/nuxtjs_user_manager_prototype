9f7760c704e64e7bfdd2b1da595cc9dd8d7b2a3a
Dice <tetradice@gmail.com>
2023-07-14 00:01:40 +0900

    Prismaアクセスコードを追記

--- web/app.vue
+++ web/app.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const allBooks = await prisma.books.findMany();
+</script>
 <template>
   <div>
     Hello, Nuxt.js
