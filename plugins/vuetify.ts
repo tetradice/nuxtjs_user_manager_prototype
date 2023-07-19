3a3b533f8dc2d92e24365ded5cfd3ed787aa0a43
Dice <tetradice@gmail.com>
2023-07-20 05:02:28 +0900

    Vuetifyを追加

---
+++ plugins/vuetify.ts
@@ -0,0 +1,16 @@
+// from <https://zenn.dev/coedo/articles/nuxt3-vuetify3>
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    ssr: true,
+    components,
+    directives,
+    // 他の設定をここに記述していく
+  });
+
+  // Vue.js で Vuetify を使用する
+  nuxtApp.vueApp.use(vuetify);
+});
