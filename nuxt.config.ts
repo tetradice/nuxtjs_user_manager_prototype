4a781a100edc437e5c741243bd11c9e432429b2b
Dice <tetradice@gmail.com>
2023-07-20 05:31:27 +0900

    mdiを最新化

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,10 @@ import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   // from <https://zenn.dev/one_dock/articles/ab6d178741956d>
-  css: ["vuetify/lib/styles/main.sass", "mdi/css/materialdesignicons.min.css"],
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css", // for mdi 7.x
+  ],
   build: {
     transpile: ["vuetify"],
   },
